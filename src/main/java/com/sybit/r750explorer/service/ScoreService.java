3abc46647ec09b4e662cee6a3c921d687100b34f
rrr <ramon.reschke@gmail.com>
2017-08-01 15:58:53 +0200

    Immer noch nix

--- src/main/java/com/sybit/r750explorer/service/ScoreService.java
+++ src/main/java/com/sybit/r750explorer/service/ScoreService.java
@@ -156,7 +156,7 @@ public class ScoreService {
         //Spielstand Löschen über spielstandRepository
         throw new MethodNotFoundException("Methode nicht implementiert");
 
-    }
+    } ////////////////////////////// TTTTTTTOOOOOOOOOOOOOODDDDDDDDDDDDDDOOOOOOOOOOOOOOOOOOOO: Außeinandersetzung mit Flo/Erik
 
     public Float hintRequested(String uuid) {
 
