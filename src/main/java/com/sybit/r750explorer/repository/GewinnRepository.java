6f8c45f8be6de3ff002496dc52f262c1735ed516
SybitGitHub2017 <Schler@nb0183.sybit.de>
2017-08-03 10:06:33 +0200

    GewinnRepository erstellt

--- src/main/java/com/sybit/r750explorer/repository/GewinnRepository.java
+++ src/main/java/com/sybit/r750explorer/repository/GewinnRepository.java
@@ -5,10 +5,41 @@
  */
 package com.sybit.r750explorer.repository;
 
+import com.sybit.airtable.Query;
+import com.sybit.airtable.exception.AirtableException;
+import com.sybit.r750explorer.exception.SpielstandSyntaxException;
+import com.sybit.r750explorer.repository.tables.Preis;
+import java.util.List;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+
 /**
  *
  * @author Schüler
  */
-public class GewinnRepository {
+@Repository
+public class GewinnRepository extends AirtableRepository {
+    
+    private final org.slf4j.Logger log = LoggerFactory.getLogger(this.getClass());
+    
+    public List<Preis> getAllPreise() {
+
+        log.debug("--> getAllPreise");
+
+        Query preisQuery = getQueryWithFilter("Status", "aktiv");
+        List<Preis> response;
+
+        try {
+            response = getAirtableBase().table("Preise", Preis.class).select(preisQuery);
+        } catch (AirtableException e) {
+
+            log.error("Error with Airtable: " + e);
+            throw new SpielstandSyntaxException("Error retrieving entrys of Preis: ");
+
+        }
+
+        log.debug("<-- getEntrysOfUUID. Retrieved Spielstand List from Airtable");
+        return response;
+    }
     
 }
